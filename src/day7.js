3a97e0ee83eb6b1d80444ec33049b17fca10cdc2
ruinedme <johndoe@example.com>
2025-12-07 22:28:33 -0600

    day 7

--- src/day7.js
+++ src/day7.js
@@ -2,6 +2,8 @@ const { Grid } = require('./utils/grid');
 
 const SPLITTER = '^'.charCodeAt(0);
 const LASER = '|'.charCodeAt(0);
+const EMPTY = '.'.charCodeAt(0);
+
 /**
  * 
  * @param {Buffer} input 
@@ -10,17 +12,16 @@ const day7_1 = (input) => {
     let total = 0;
     const grid = new Grid(input);
     const beams = [];
-    const usedSpaces = [];
+    const forks = [];
     beams.push(grid.grid.indexOf('S'));
+    const start = beams[0];
 
-    // let iters = 0;
     while (beams.length > 0) {
-        // iters++;
         const index = beams.length - 1;
         let beam = beams[index];
 
         // Already traversed this path
-        if(usedSpaces.includes(beam)){
+        if (forks.includes(beam)) {
             beams.pop();
             continue;
         }
@@ -30,7 +31,6 @@ const day7_1 = (input) => {
         // Beam has exited the grid
         if (row + 1 === grid.height) {
             const done = beams.pop();
-            // console.log('finished beaam', done);
             continue;
         }
 
@@ -38,9 +38,8 @@ const day7_1 = (input) => {
         const nextRow = grid.getIndex(row + 1, col);
         if (grid.grid[nextRow] === SPLITTER) {
             const b = beams.pop();
-            if (!usedSpaces.includes(b)){
-                usedSpaces.push(b);
-                total++;
+            if (!forks.includes(b)) {
+                forks.push(b);
             }
             // Need to worry about left/right edge grid?
             // Looks like there is a 1 column gap on both sides, and whole bottom row is empty
@@ -52,9 +51,9 @@ const day7_1 = (input) => {
         // beam continues down
         beams[index] = nextRow;
     }
-    // grid.display();
-    // console.log('iters?', iters);
-    
+
+    grid.grid[start] = 'S'.charCodeAt(0);
+    total = forks.length;
     console.log(`Answer: ${total}`);
 };
 
@@ -64,6 +63,23 @@ const day7_1 = (input) => {
  */
 const day7_2 = (input) => {
     let total = 0;
+
+    // The structure of the beams is simialar to pascal's triangle
+    //          1       .......|........
+    //         1 1      ......|^|.......
+    //        1 2 1     .....|^|^|......
+    //       1 3 3 1    ....|^|^|^|.....
+    //      1 4 3 1 1   ...|^|^|||^|.... // This is where it deviates
+    //           3         1 4 331 1
+    //                          ^ This 3 is carried down from the previous row
+    // Except there are some gaps, not every splitter causes beams to overlap
+    // And in certain cases some splitters are never hit by a beam directly
+    // From the start point bing the laser down to the next row
+    // From every splitter on each row check if there is a laser above it
+    // If there is a laser above the splitter make 2 new lasers to the left and right of the splitter
+    // How to preserve the counts of the inner numbers between rows?
+    let lines = input.toString().split('\r\n');
+    const start = lines[0].indexOf('S');
 
     console.log(`Answer: ${total}`);
 };
